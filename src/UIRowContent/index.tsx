dcd8ebf6f362ed387343f1ce42b0b3be8a08c23b
Silic1 <silic1@hotmail.com>
2020-07-02 18:06:32 +0200

    Image Row content

--- src/UIRowContent/index.tsx
+++ src/UIRowContent/index.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
-import { Row, isHeadingRow, isDescriptionRow } from "@cyoc/model";
+import { Row, isHeadingRow, isDescriptionRow, isImageRow } from "@cyoc/model";
 import { Heading } from "./Heading";
 import { Description } from "./Description";
+import { Image } from "./Image";
 
 export * from "./Heading";
 
 const contentMap: Array<(row: Row) => JSX.Element | null> = [
 	r => isHeadingRow(r) ? <Heading {...r}/> : null,
-	r => isDescriptionRow(r) ? <Description {...r}/> : null
+	r => isDescriptionRow(r) ? <Description {...r}/> : null,
+	r => isImageRow(r) ? <Image {...r}/> : null
 ];
 
 export function GetContent(row: Row): JSX.Element {
