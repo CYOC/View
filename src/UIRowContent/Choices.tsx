855f73dae209eabcd1f8ad15b4c9fa25bde21eb0
Silic1 <silic1@hotmail.com>
2020-07-04 16:30:33 +0200

    Flex styling for choice rows and choices

--- src/UIRowContent/Choices.tsx
+++ src/UIRowContent/Choices.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from "react";
+import { Component, ReactNode, CSSProperties } from "react";
 import * as React from "react";
 import { ChoiceRow } from "@cyoc/model";
 import { UIChoice } from "./UIChoice";
@@ -7,11 +7,25 @@ interface ChoicesState {
 }
 
 export class Choices extends Component<ChoiceRow, ChoicesState> {
+	private static readonly choiceContainerStyle: Readonly<CSSProperties> = {
+		display: "flex",
+		flexWrap: "wrap",
+		justifyContent: "space-evenly",
+		alignItems: "flex-start"
+	};
+	private static readonly choiceStyle: Readonly<CSSProperties> = {
+		flexGrow: 1
+	};
+
 	public constructor(props: Readonly<ChoiceRow>) {
 		super(props);
 	}
 
 	public render(): ReactNode {
-		return this.props.choices.map(p => <UIChoice key={p.name} {...p}/>);
+		return (
+			<div style={Choices.choiceContainerStyle}>
+				{ this.props.choices.map(p => <UIChoice key={p.name} style={Choices.choiceStyle} {...p}/>) }
+			</div>
+		);
 	}
 }
--- src/UIRowContent/UIChoice.tsx
+++ src/UIRowContent/UIChoice.tsx
@@ -1,18 +1,21 @@
-import { Component, ReactNode } from "react";
+import { Component, ReactNode, CSSProperties } from "react";
 import * as React from "react";
 import { Choice } from "@cyoc/model";
 
+type UIChoiceProperties = Choice & {
+	style?: CSSProperties
+};
 interface UIChoiceState {
 }
 
-export class UIChoice extends Component<Choice, UIChoiceState> {
-	public constructor(props: Readonly<Choice>) {
+export class UIChoice extends Component<UIChoiceProperties, UIChoiceState> {
+	public constructor(props: Readonly<UIChoiceProperties>) {
 		super(props);
 	}
 
 	public render(): ReactNode {
 		return (
-			<p>{this.props.name}</p>
+			<p style={this.props.style}>{this.props.name}</p>
 		);
 	}
 }
